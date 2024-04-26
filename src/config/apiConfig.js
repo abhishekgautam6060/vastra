e5d1b3b48ee533dc9bf2c5ee83e8f3141c68714b
abhishekgautam6060 <111275009+abhishekgautam6060@users.noreply.github.com>
2024-04-26 15:19:44 +0530

    Update apiConfig.js

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8084";
+export const API_BASE_URL = "https://e-commerce-backend-production-3965.up.railway.app";
 
 const jwt = localStorage.getItem("jwt");
 
